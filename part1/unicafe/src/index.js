838e2d10d92fe2bbbf57341f035f67f966d86c9f
LBeul <l.beul@outlook.de>
2019-10-14 00:45:46 +0200

    1.6: unicafe step1

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,12 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const App = () => {
+  // State initialization
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  // Event listeners
+  const onGoodClick = () => {
+    setGood(good + 1);
+  };
+  const onNeutralClick = () => {
+    setNeutral(neutral + 1);
+  };
+  const onBadClick = () => {
+    setBad(bad + 1);
+  };
+
+  return (
+    <>
+      <h1>give feedback</h1>
+      <button onClick={() => onGoodClick()}>Good</button>
+      <button onClick={() => onNeutralClick()}>Neutral</button>
+      <button onClick={() => onBadClick()}>Bad</button>
+      <h1>statistics</h1>
+      <p>Good: {good}</p>
+      <p>Neutral: {neutral}</p>
+      <p>Bad: {bad}</p>
+    </>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
