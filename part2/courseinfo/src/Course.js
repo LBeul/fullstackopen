12ebc11324f69fe1f24bf6499f68907a99e736ab
LBeul <l.beul@outlook.de>
2019-11-05 23:03:58 +0100

    2.4: Course contents step9 & 2.5: separate module

--- part2/courseinfo/src/Course.js
+++ part2/courseinfo/src/Course.js
@@ -3,14 +3,17 @@ import Header from "./Header";
 import Content from "./Content";
 import Total from "./Total";
 
-const Course = ({ course }) => {
-  return (
-    <>
-      <Header courseName={course.name} />
-      <Content partsList={course.parts} />
-      <Total partsList={course.parts} />
-    </>
-  );
+const Course = ({ courses }) => {
+  const listOfCourses = () =>
+    courses.map((course, i) => (
+      <div key={i}>
+        <Header courseName={course.name} />
+        <Content partsList={course.parts} />
+        <Total partsList={course.parts} />
+      </div>
+    ));
+
+  return <>{listOfCourses()}</>;
 };
 
 export default Course;
--- part2/courseinfo/src/Total.js
+++ part2/courseinfo/src/Total.js
@@ -4,7 +4,11 @@ const Total = ({ partsList }) => {
   const exercisesTotal = partsList
     .map(element => element.exercises)
     .reduce((accumulator, currentValue) => accumulator + currentValue);
-  return <p>Number of exercises {exercisesTotal}</p>;
+  return (
+    <>
+      <strong>total of {exercisesTotal} exercises</strong>
+    </>
+  );
 
   // return <p>Number of Exercises {exercisesTotal}</p>;
 };
