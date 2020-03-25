5cd5b448abb0ac66f388f0977007b9a0553ccf09
LBeul <l.beul@outlook.de>
2020-03-25 16:55:45 +0100

    2.8: The Phonebook Step3

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,28 +2,55 @@ import React, { useState } from "react";
 
 const App = () => {
   // Initilaize the component's state
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "0170333555" }
+  ]);
   const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
 
-  // Event handlers
+  // Input handlers
   const handleNameChange = event => {
-    console.log(event.target.value);
+    console.log("name:", event.target.value);
     setNewName(event.target.value);
   };
 
-  const handleNameSubmit = event => {
+  const handleNumberChange = event => {
+    console.log("number:", event.target.value);
+    setNewNumber(event.target.value);
+  };
+
+  // Submit handler
+  const handleFormSubmit = event => {
     event.preventDefault();
-    if (persons.filter(item => item.name === newName).length > 0) {
-      alert(`Sorry, but ${newName} is already added to phonebook.`);
-    } else {
+
+    // Check if unique
+    let personIsUnique = false;
+    let numberIsUnique = false;
+    if (persons.filter(item => item.name === newName).length === 0) {
+      personIsUnique = true;
+    }
+    if (persons.filter(item => item.number === newNumber).length === 0) {
+      numberIsUnique = true;
+    }
+
+    // If person & number are unique...
+    if (personIsUnique && numberIsUnique) {
+      // ...add the new person object
       const nameObject = {
         name: newName,
-        id: newName
+        number: newNumber
       };
-
       setPersons(persons.concat(nameObject));
+    } else if (!personIsUnique) {
+      // Feedback if person already exists
+      alert(`Sorry, but ${newName} is already added to phonebook.`);
+    } else if (!numberIsUnique) {
+      // Feedback if number already exists
+      alert(`Sorry, but ${newNumber} is already assigned to someone else.`);
     }
+    // Clear inout fields afterwards
     setNewName("");
+    setNewNumber("");
   };
 
   // Render the app component
@@ -35,7 +62,10 @@ const App = () => {
           name: <input value={newName} onChange={handleNameChange} />
         </div>
         <div>
-          <button type="submit" onClick={handleNameSubmit}>
+          number: <input value={newNumber} onChange={handleNumberChange} />
+        </div>
+        <div>
+          <button type="submit" onClick={handleFormSubmit}>
             add
           </button>
         </div>
@@ -43,7 +73,9 @@ const App = () => {
       <h2>Numbers</h2>
       <ul>
         {persons.map(person => (
-          <li key={person.name}>{person.name}</li>
+          <li key={person.name}>
+            <strong>{person.name}</strong> {person.number}
+          </li>
         ))}
       </ul>
     </>
